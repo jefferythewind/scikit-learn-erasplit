5cb490dafa91d1328b46936605cb41b996678fa5
Tim DeLise <tdelise@gmail.com>
2023-05-05 15:38:05 -0400

    take out prints

--- sklearn/ensemble/_hist_gradient_boosting/gradient_boosting.py
+++ sklearn/ensemble/_hist_gradient_boosting/gradient_boosting.py
@@ -672,7 +672,7 @@ class BaseHistGradientBoosting(BaseEstimator, ABC):
 
             # Build `n_trees_per_iteration` trees.
             for k in range(self.n_trees_per_iteration_):
-                print( "residual size: ", np.mean( np.abs(g_view[:, k]) ) )
+                #print( "residual size: ", np.mean( np.abs(g_view[:, k]) ) )
                 grower = TreeGrower(
                     X_binned=X_binned_train,
                     gradients=g_view[:, k],
@@ -2603,7 +2603,7 @@ class BaseEraHistGradientBoosting(BaseEstimator, ABC):
 
             # Build `n_trees_per_iteration` trees.
             for k in range(self.n_trees_per_iteration_):
-                print( "residual size: ", np.mean( np.abs(g_view[:, k]) ) )
+                #print( "residual size: ", np.mean( np.abs(g_view[:, k]) ) )
                 grower = EraTreeGrower(
                     X_binned=X_binned_train,
                     gradients=g_view[:, k],
